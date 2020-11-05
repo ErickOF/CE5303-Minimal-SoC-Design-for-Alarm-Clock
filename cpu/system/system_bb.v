399a223f7f7c393ed13155cc426ae4985cd58281
Ki Sung Lim <kisungqueso@hotmail.com>
2020-11-05 08:06:36 -0600

    [Add/Update] Add Timer and conexions update

--- cpu/system/system_bb.v
+++ cpu/system/system_bb.v
@@ -3,28 +3,28 @@ module system (
 	alarm_export,
 	btn_down_export,
 	btn_set_alarm_export,
+	btn_set_clock_export,
 	btn_up_export,
 	clk_clk,
-	display_h0_export,
-	display_h1_export,
 	display_m0_export,
 	display_m1_export,
 	display_s0_export,
 	display_s1_export,
-	reset_reset_n,
-	btn_set_clock_export);	
+	display_h0_export,
+	display_h1_export,
+	reset_reset_n);	
 
 	output		alarm_export;
 	input		btn_down_export;
 	input		btn_set_alarm_export;
+	input		btn_set_clock_export;
 	input		btn_up_export;
 	input		clk_clk;
-	output	[3:0]	display_h0_export;
-	output	[3:0]	display_h1_export;
 	output	[3:0]	display_m0_export;
 	output	[3:0]	display_m1_export;
 	output	[3:0]	display_s0_export;
 	output	[3:0]	display_s1_export;
+	output	[3:0]	display_h0_export;
+	output	[3:0]	display_h1_export;
 	input		reset_reset_n;
-	input		btn_set_clock_export;
 endmodule
--- cpu/system/system_inst.vhd
+++ cpu/system/system_inst.vhd
@@ -3,16 +3,16 @@
 			alarm_export         : out std_logic;                           -- export
 			btn_down_export      : in  std_logic                    := 'X'; -- export
 			btn_set_alarm_export : in  std_logic                    := 'X'; -- export
+			btn_set_clock_export : in  std_logic                    := 'X'; -- export
 			btn_up_export        : in  std_logic                    := 'X'; -- export
 			clk_clk              : in  std_logic                    := 'X'; -- clk
-			display_h0_export    : out std_logic_vector(3 downto 0);        -- export
-			display_h1_export    : out std_logic_vector(3 downto 0);        -- export
 			display_m0_export    : out std_logic_vector(3 downto 0);        -- export
 			display_m1_export    : out std_logic_vector(3 downto 0);        -- export
 			display_s0_export    : out std_logic_vector(3 downto 0);        -- export
 			display_s1_export    : out std_logic_vector(3 downto 0);        -- export
-			reset_reset_n        : in  std_logic                    := 'X'; -- reset_n
-			btn_set_clock_export : in  std_logic                    := 'X'  -- export
+			display_h0_export    : out std_logic_vector(3 downto 0);        -- export
+			display_h1_export    : out std_logic_vector(3 downto 0);        -- export
+			reset_reset_n        : in  std_logic                    := 'X'  -- reset_n
 		);
 	end component system;
 
@@ -21,15 +21,15 @@
 			alarm_export         => CONNECTED_TO_alarm_export,         --         alarm.export
 			btn_down_export      => CONNECTED_TO_btn_down_export,      --      btn_down.export
 			btn_set_alarm_export => CONNECTED_TO_btn_set_alarm_export, -- btn_set_alarm.export
+			btn_set_clock_export => CONNECTED_TO_btn_set_clock_export, -- btn_set_clock.export
 			btn_up_export        => CONNECTED_TO_btn_up_export,        --        btn_up.export
 			clk_clk              => CONNECTED_TO_clk_clk,              --           clk.clk
-			display_h0_export    => CONNECTED_TO_display_h0_export,    --    display_h0.export
-			display_h1_export    => CONNECTED_TO_display_h1_export,    --    display_h1.export
 			display_m0_export    => CONNECTED_TO_display_m0_export,    --    display_m0.export
 			display_m1_export    => CONNECTED_TO_display_m1_export,    --    display_m1.export
 			display_s0_export    => CONNECTED_TO_display_s0_export,    --    display_s0.export
 			display_s1_export    => CONNECTED_TO_display_s1_export,    --    display_s1.export
-			reset_reset_n        => CONNECTED_TO_reset_reset_n,        --         reset.reset_n
-			btn_set_clock_export => CONNECTED_TO_btn_set_clock_export  -- btn_set_clock.export
+			display_h0_export    => CONNECTED_TO_display_h0_export,    --    display_h0.export
+			display_h1_export    => CONNECTED_TO_display_h1_export,    --    display_h1.export
+			reset_reset_n        => CONNECTED_TO_reset_reset_n         --         reset.reset_n
 		);
 
